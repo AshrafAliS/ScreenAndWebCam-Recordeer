76cbb48668cf4079938afeaab71540feaf49e6ad
Ashraf Ali <fashionzone004@gmail.com>
2020-06-28 17:12:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -267,7 +267,7 @@ if ((!empty($_GET['name'])) and (!empty($_GET['usn']))) {
                                             echo "temp.webm";
                                         }
                                         ?>
-                        //var fileName = getFileName('webm');
+			    
                         console.log(fileName);
                         
                         // we need to upload "File" --- not "Blob"
@@ -343,28 +343,6 @@ if ((!empty($_GET['name'])) and (!empty($_GET['usn']))) {
                 callback = function() {};
             }, false);
         });
-    }
-    
-    // this function is used to generate random file name
-    function getFileName(fileExtension) {
-        var d = new Date();
-        var year = d.getUTCFullYear();
-        var month = d.getUTCMonth();
-        var date = d.getUTCDate();
-        return 'RIT-' + year + month + date + '-' + getRandomString() + '.' + fileExtension;
-    }
-    
-    function getRandomString() {
-        if (window.crypto && window.crypto.getRandomValues && navigator.userAgent.indexOf('Safari') === -1) {
-            var a = window.crypto.getRandomValues(new Uint32Array(3)),
-                token = '';
-            for (var i = 0, l = a.length; i < l; i++) {
-                token += a[i].toString(36);
-            }
-            return token;
-        } else {
-            return (Math.random() * new Date().getTime()).toString(36).replace(/\./g, '');
-        }
     }
     
     
